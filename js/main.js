894297062634fdfd0f2b7d78be72845f05e024b0
Jacob Seiler <seiler.jacob1@gmail.com>
2019-08-19 20:59:25 -0400

    Choice amount and min selections now adjustable

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
-const options = document.querySelectorAll(".option");
+// Variables
+const choiceAmount = 2;
+const minResponses = 3;
+
+const buttonResults = document.querySelector(".button-results");
 let selections = [];
 
-function updateSelections() {
+function updateOptions(options) {
+	// TODO remove on click event until new stuff comes in
 	// TODO get new options from server, then update DOM
+
+	if (selections.length >= minResponses) {
+		buttonResults.classList.remove("hidden");
+	}
+
+	/*
+	<div class="option option-1" style="background-image: url('temp-1.png')">
+		<div class="option-content">
+			<h2>First Last</h2>
+			<p>Computing</p>
+			<p>3 years</p>
+		</div>
+    </div>;
+    */
+
+	for (let i = 0; i < options.length; i++) {
+		let element = options[i].children[0];
+		element.innerHTML = "";
+
+		let title = document.createElement("H2");
+		title.textContent = "First Last";
+
+		let sub1 = document.createElement("P");
+		sub1.textContent = "Computing";
+
+		let sub2 = document.createElement("P");
+		sub2.textContent = "3 years";
+
+		element.appendChild(title);
+		element.appendChild(sub1);
+		element.appendChild(sub2);
+	}
 }
 
 function getResults() {
@@ -10,14 +47,31 @@ function getResults() {
 	// TODO error from server if selections is less than 3
 }
 
+function generateOptions() {
+	const choices = document.querySelector(".choices");
+
+	for (let i = 0; i < choiceAmount; i++) {
+		let outer = document.createElement("DIV");
+		outer.classList.add("option");
+		outer.classList.add("option-" + (i + 1));
+		let inner = document.createElement("DIV");
+		inner.classList.add("option-content");
+		outer.appendChild(inner);
+		choices.appendChild(outer);
+	}
+
+	return document.querySelectorAll(".option");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-	updateSelections();
+	const options = generateOptions();
+	updateOptions(options);
 
 	options.forEach(option => {
 		option.addEventListener("click", e => {
 			console.log(e.target);
 			selections.push(e.target);
-			updateSelections();
+			updateOptions(options);
 		});
 	});
 });
